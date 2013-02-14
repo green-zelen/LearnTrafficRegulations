e8ef0435d19eb1459e34254f27d7492dd01d634c
green-zelen <marigreenzelen@gmail.com>
2013-02-15 01:23:31 +1100

    first experiments

---
+++ pdd/otkr.js
@@ -0,0 +1,12 @@
+
+function otkr() {
+	var chapter1=document.getElementById('chapter1');
+	if (chapter1.style.display=='none')
+	{
+		chapter1.style.display='block';	
+	}
+	else 
+	{
+		chapter1.style.display='none';
+	}
+}
